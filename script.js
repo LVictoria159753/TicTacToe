591fde9e5f7ef0f9053c68a0f3bbffe5319b3fa2
Victoria <Lvictoria159753@gmail.com>
2022-02-10 01:06:16 -0800

    added pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+let gameboard= ["X", "O", "X", "O"];
+
+let player1;
+let player2;
+
+
+
+//create a grid
+//style gird so it appears like a TicTacBoard
+//Player 1, Player 2 or Player 1 and computer setup prompt
+// store player 1 and player 2 move 
+// insert the combinations of winners  vs losers
+//declare on the screen who wins or not. 
